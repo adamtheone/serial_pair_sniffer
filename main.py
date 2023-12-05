e6c9e43e08e7f541d9b1ef2941981691816427a1
Adam Varga <dipcanmod@gmail.com>
2023-12-05 22:42:31 +0100

    continous reconnecting

--- main.py
+++ main.py
@@ -25,14 +25,22 @@ def main():
     serial2.port = PORT2_NAME
     serial2.baudrate = PORT2_BAUD
     serial2.timeout = 0.001
-    try:
-        serial1.open()
-        serial2.open()
-    except serial.serialutil.SerialException as e:
-        print(f'Cannot open the serial port(s): {PORT1_NAME}, {PORT2_NAME}. Error: {e}')
-        return
 
-    print(f'Connected to ports: {PORT1_NAME}, {PORT2_NAME}')
+    def serial_connect():
+        serial1.close()
+        serial2.close()
+        while (not serial1.is_open) or (not serial2.is_open):
+            try:
+                if not serial1.is_open:
+                    serial1.open()
+                if not serial2.is_open:
+                    serial2.open()
+            except serial.serialutil.SerialException as e:
+                print(f'Cannot open the serial port: {PORT1_NAME}, {PORT2_NAME}. Error: {e}')
+                time.sleep(0.5)
+        print(f'Connected to ports: {PORT1_NAME}, {PORT2_NAME}')
+
+    serial_connect()
 
     last_rx_time = time.time()
     while True:
@@ -49,8 +57,9 @@ def main():
                 last_rx_time = time.time()
                 if PRINTING_ENABLED:
                     print(f'[{datetime.now().strftime("%H:%M:%S.%f")[:-3]}] << {data2}')    # .decode("utf-8")}')
-        except:
-            pass
+        except Exception as e:
+            print(e)
+            serial_connect()
 
         if last_rx_time and time.time() - last_rx_time > 0.5:
             last_rx_time = 0
